818712994965902f23b2971393061b07932ced0f
garethosullivan <gareth.osullivan@icloud.com>
2018-03-27 21:22:25 +0100

    Added the Play Sounds View Controller Class

---
+++ PitchPerfectUdacityProject/PlaySoundsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfectUdacityProject
+//
+//  Created by Gareth O'Sullivan on 27/03/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- PitchPerfectUdacityProject/RecordSoundsViewController.swift
+++ PitchPerfectUdacityProject/RecordSoundsViewController.swift
@@ -22,11 +22,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         super.viewDidLoad()
         configureUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     //MARK:- UI Buttons
 
