2eee8760042209effc26ec29c4da89931adc9e91
garethosullivan <gareth.osullivan@icloud.com>
2018-03-27 19:53:12 +0100

    Initial Commit

---
+++ PitchPerfectUdacityProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfectUdacityProject
+//
+//  Created by Gareth O'Sullivan on 27/03/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
